8ac1ab4c558a753fc1e929cc4ee37da0fb2f40f5
kLawrence <ultimate1352@gmail.com>
2018-06-24 17:04:48 +0900

    create share module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ShareModule } from './share/share.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/share/share.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NgbModule
+  ],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent]
+})
+export class ShareModule { }

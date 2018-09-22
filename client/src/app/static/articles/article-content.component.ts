d6e8147785940e0ec32de370f3ef025b5ec1c36d
kLawrence <ultimate1352@gmail.com>
2018-09-22 20:45:10 +0900

    link to article service

--- client/src/app/static/articles/article-content.component.ts
+++ client/src/app/static/articles/article-content.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { ArticleService } from './articles.service';
+
 @Component({
   selector: 'anms-article-content',
   templateUrl: './article-content.component.html',
-  styleUrls: ['./article-content.component.css']
+  styleUrls: ['./article-content.component.scss']
 })
 export class ArticleContentComponent implements OnInit {
 
-  title = '스칼라 언어 사용하기'
-  
-  content = `# hello world
-  | a | b | c | d |
-  |---|---|---|---|
-  | e | f | g | h |`
-
-  constructor() { }
-
-  ngOnInit() {
+  article = {
+    title: '',
+    content: ''
   }
 
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private articleService: ArticleService) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.articleService.getById(id).subscribe(response =>
+      this.article = response
+    )
+  }
 }
--- server/app/services/ArticleService.scala
+++ server/app/services/ArticleService.scala
@@ -15,29 +15,7 @@ import models.Article
 @Singleton
 class ArticleService @Inject()() {
 
-/*
-  articles = [
-    {
-      'id': '1',
-      'title': '스칼라 언어에서 트레이트 사용하기',
-      'content': '트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다.',
-      'date': new Date,
-      'author': 'Lawrence Kim',
-      'category': ['Scala', 'basic', 'trait', 'ddd'],
-    },
-    {
-      'id': '2',
-      'title': '이 번주 스칼라 소식',
-      'content': '1. 스파크 속 아카 이야기, 2. 스칼라 데이즈 현장 취재',
-      'date': new Date,
-      'author': 'Lawrence Kim',
-      'category': ['Scala', 'Spark', 'Akka', 'ScalaDays2018']
-    }
-  ]
- */
-
-
-  def get: List[Article] = List(
+  val articles = List(
     Article(id = 1,
       title = "스칼라 언어에서 트레이트 사용하기",
       content = "트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다.",
@@ -54,12 +32,8 @@ class ArticleService @Inject()() {
     )
   )
 
-  def getById(id: Long): Article = Article(id = 1,
-    title = "",
-    content = "",
-    date = LocalDateTime.now,
-    author = "",
-    category = List.empty
-  )
+  def get: List[Article] = articles
+
+  def getById(id: Long): Article = articles.filter(_.id == id).head
 
 }

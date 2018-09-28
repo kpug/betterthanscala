c97121c6085c142b8c60e2f0099163f9656472db
kLawrence <ultimate1352@gmail.com>
2018-09-28 23:26:56 +0900

    resolve animation

--- client/src/app/static/articles/articles.component.ts
+++ client/src/app/static/articles/articles.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Optional } from '@angular/core';
 import { ArticleService, Article } from './articles.service';
 
+import { ROUTE_ANIMATIONS_ELEMENTS } from '@app/core';
 @Component({
   selector: 'anms-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
 export class ArticlesComponent implements OnInit {
-
+  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   articles: Array<Article>
 
   constructor(private articleService: ArticleService) { }

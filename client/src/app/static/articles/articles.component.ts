d873af079a288d97932d558187afa1661ec027a8
kLawrence <ultimate1352@gmail.com>
2018-11-05 21:26:17 +0900

    develop pagenation

--- client/src/app/static/articles/articles.component.ts
+++ client/src/app/static/articles/articles.component.ts
@@ -4,7 +4,8 @@ import { ArticleService, Article } from './articles.service';
 import { ROUTE_ANIMATIONS_ELEMENTS } from '@app/core';
 import { ActivatedRoute } from '@angular/router';
 import { convertPropertyBindingBuiltins } from '@angular/compiler/src/compiler_util/expression_converter';
-
+import { FormArrayName } from '@angular/forms';
+import * as _ from 'lodash';
 @Component({
   selector: 'anms-articles',
   templateUrl: './articles.component.html',
@@ -16,20 +17,23 @@ export class ArticlesComponent implements OnInit {
   pages: number;
   start: number;
   end: number;
+  pageIndexes;
 
   constructor(private articleService: ArticleService,
     private route: ActivatedRoute) {
     }
 
   ngOnInit() {
-    const [articles, total] = this.route.snapshot.data.response;
+    const [articles, totalCount] = this.route.snapshot.data.response;
+    const countPerPage = 5;
+    const totalPages = Math.ceil(totalCount / countPerPage);
+
     this.articles = articles;
     this.pages = this.getPages();
-    this.start = this.calcStartPages(this.pages, total);
-    this.end = this.calcEndPages(this.pages, total);
-    console.log(this.pages, this.start, this.end);
+    this.start = this.calcStartPages(this.pages, totalPages);
+    this.end = this.calcEndPages(this.pages, totalPages);
+    this.pageIndexes = _.range(this.start, this.end + 1);
   }
-
 
   getPages() {
     const pages = +this.route.snapshot.queryParams.pages;
@@ -54,21 +58,33 @@ export class ArticlesComponent implements OnInit {
     } else if (pages <= 2) {
       return 5;
     } else if ( pages >= total - 1 ) {
-      return total;
+      return total / 5;
     } else {
       return pages + 2;
     }
   }
 
-  previousClick() {
-    console.log('previous Click');
+  /**
+   * onClick: 이전 버튼
+   */
+  onClickPreviousArrow() {
+    this.pageIndexes = _.range(this.start, this.end + 1);
   }
 
-  nextClick() {
-    console.log('next Click');
+  /**
+   * onClick: 다음 버튼
+   */
+  onClickNextArrow() {
+    this.start = this.start + 5;
+    this.end = this.end + 5;
+    this.pageIndexes = _.range(this.start, this.end + 1);
   }
 
-  loadPage(number) {
+  /**
+   * onClick: 페이징 숫자
+   * @param number
+   */
+  onClickReloadPage(number) {
     console.log(number);
   }
 
